6400046244cb0789648368ad6551faf1e21db2c0
ElighAlvarez <112344852+ElighAlvarez@users.noreply.github.com>
2025-06-04 21:16:10 -0500

    ParentNode and textnode to leafnode

---
+++ src/parentnode.py
@@ -0,0 +1,13 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("parent nodes must have a tag")
+        if self.children == None:
+            raise ValueError("parent nodes must have children")
+        children_html = "".join(list(map(lambda child: child.to_html(), self.children)))
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -18,6 +18,11 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.IMAGE, '../static/shiba.png')
         node2 = TextNode("This is a text node", TextType.IMAGE, '../static/doge.png')
         self.assertNotEqual(node, node2)
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.NORMAL)
+        html_node = node.to_html_node()
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
