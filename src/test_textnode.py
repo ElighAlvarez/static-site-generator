ac1a98ae6b8a0ddd17d75637b2fd4b6f191819ef
ElighAlvarez <112344852+ElighAlvarez@users.noreply.github.com>
2025-06-04 20:10:18 -0500

    Update test_textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -6,6 +6,19 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+    def test_different_style(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+    def test_different_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is also a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    def test_different_url(self):
+        node = TextNode("This is a text node", TextType.IMAGE, '../static/shiba.png')
+        node2 = TextNode("This is a text node", TextType.IMAGE, '../static/doge.png')
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
