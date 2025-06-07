b6eecee69f7ec3e8584f98524c2d5dbdea4a3158
ElighAlvarez <112344852+ElighAlvarez@users.noreply.github.com>
2025-06-07 17:07:52 -0500

    Generalized build location. Added build.sh for GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-from utilities import copy_static_to_public, generate_pages_recursive
+from utilities import copy_static_to_dir, generate_pages_recursive
+import sys
 
 def main():
-    copy_static_to_public()
-    generate_pages_recursive("content", "template.html", "public")
+    if len(sys.argv) == 2:
+        root = sys.argv[1]
+    else:
+        root = "/"
+
+    public_dir = "docs"
+    copy_static_to_dir(public_dir)
+    generate_pages_recursive(root, "content", "template.html", public_dir)
 
 if __name__ == '__main__':
     main()
--- src/utilities.py
+++ src/utilities.py
@@ -10,10 +10,10 @@ def markdown_to_html_node(markdown):
     return ParentNode("div", html_nodes)
 
 # overwrite the contents of the public directory with the static directory
-def copy_static_to_public():
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    shutil.copytree("static", "public")
+def copy_static_to_dir(dir):
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+    shutil.copytree("static", dir)
 
 # returns the text of the first h1 header
 def extract_title(markdown):
@@ -23,7 +23,7 @@ def extract_title(markdown):
             return block[2:]
     raise Exception("no h1 header found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(root, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         markdown = f.read()
@@ -32,7 +32,11 @@ def generate_page(from_path, template_path, dest_path):
 
     title = extract_title(markdown)
     body_html = markdown_to_html_node(markdown).to_html()
-    output = template.replace("{{ Title }}", title).replace("{{ Content }}", body_html)
+    output = template \
+        .replace("{{ Title }}", title) \
+        .replace("{{ Content }}", body_html) \
+        .replace("href=\"/", f"href=\"{root}") \
+        .replace("src=\"/", f"src=\"{root}")
 
     dest_dir = os.sep.join(dest_path.split(os.sep)[:-1])
     if not os.path.exists(dest_dir):
@@ -40,13 +44,13 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as f:
         f.write(output)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(root, dir_path_content, template_path, dest_dir_path):
     dir_contents = os.listdir(dir_path_content)
     for item in dir_contents:
         item_path = f"{dir_path_content}{os.sep}{item}"
         dest_path = f"{dest_dir_path}{os.sep}{item}"
         if os.path.isdir(item_path):
             os.mkdir(dest_path)
-            generate_pages_recursive(item_path, template_path, dest_path)
+            generate_pages_recursive(root, item_path, template_path, dest_path)
         else:
-            generate_page(item_path, template_path, dest_path.replace(".md", ".html"))
+            generate_page(root, item_path, template_path, dest_path.replace(".md", ".html"))
